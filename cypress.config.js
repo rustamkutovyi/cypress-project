726e6c3c73ce8a2b8c9b1b45b1ac195d9f1560a4
rustamkutovyi <rustamkutovoy996@gmail.com>
2024-03-27 20:48:35 -0700

    beforeHook was implemented

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,9 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    excludeSpecPattern: [
+        'cypress/e2e/1-getting-started/*',
+        'cypress/e2e/2-advanced-examples/*',
+    ]
   },
 });
--- cypress/e2e/auth.cy.js
+++ cypress/e2e/auth.cy.js
@@ -1,26 +1,29 @@
 describe('Auth', () => {
-    it('Sign in with valid credentials', () => {
+    beforeEach(() => {
         cy.visit('https://coding.pasv.us/user/login')
+    })
+    it('Sign in with valid credentials', () => {
+
         cy.get('#normal_login_email').type('test1@example.com')
         cy.get('#normal_login_password').type('Qwerty1234')
         cy.get('button[type="submit"]').click()
         cy.get('.ant-avatar-square').should('be.visible')
     })
     it('Sign in with invalid email credential', () => {
-        cy.visit('https://coding.pasv.us/user/login')
+
         cy.get('#normal_login_email').type('test1111@example.com')
         cy.get('#normal_login_password').type('Qwerty1234')
         cy.get('button[type="submit"]').click()
         cy.get('.ant-notification-notice-message').should('be.visible').and('have.text', 'User login. Fail')
     });
     it('Check if button Войти is enabled if both email and password fields are not empty', () => {
-        cy.visit('https://coding.pasv.us/user/login')
+
         cy.get('#normal_login_email').type('test1@example.com')
         cy.get('#normal_login_password').type('Qwerty1234')
         cy.get('button[type="submit"]').should('be.enabled')
     })
     it('Check if Required is visible', () => {
-        cy.visit('https://coding.pasv.us/user/login')
+
         cy.get('#normal_login_email').type('test1@example.com').clear()
         // cy.get('.ant-form-item-explain-error').should('be.visible').and('have.text', 'Required')
         cy.contains('.ant-form-item-explain-error','Required')
@@ -28,7 +31,7 @@ describe('Auth', () => {
         cy.get('.ant-form-item-explain-error').should('have.css', 'color', 'rgb(255, 77, 79)')
     });
     it('Sign in for validation', () => {
-        cy.visit('https://coding.pasv.us/user/login')
+
         cy.get('#normal_login_email').should('have.value', '')
         cy.get('#normal_login_password').should('have.value', '')
         cy.get('button[type="submit"]').should('be.disabled')
